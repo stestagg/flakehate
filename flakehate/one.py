328efa2046e2002eaca6ff91552465167a9aa540
Robert Scott <code@humanleg.org.uk>
2017-02-02 21:07:36 +0000

    worst code ever.

--- flakehate/one.py
+++ flakehate/one.py
@@ -3,9 +3,17 @@ from flake8.checker import FileChecker
 
 class PluginOne(FileChecker):
 
-	name = "flakehate"
-	version = "666"
+    name = "flakehate"
+    version = "666"
+    def __init__(self, tree, filename):
+        self.filename = filename
 
-	def __init__(self, *a, **kw):
-		print(a, kw)
+    def run(self):
+        lengths = set()
+        with open(self.filename) as myfile:
+            for i, line in enumerate(myfile.readlines()):
+                if len(line) in lengths:
+                    yield i, 0, "Already have line of length {}".format(len(line)), type(self)
+                else:
+                    lengths.add(len(line))
 
